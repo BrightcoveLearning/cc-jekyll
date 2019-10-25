d6d91f236c9b2ab05c8974f1a7c90811c7757577
Robert Crooks <rcrooks@brightcove.com>
2019-10-25 12:29:06 -0400

    wip

--- _site/assets/js/setDate.js
+++ _site/assets/js/setDate.js
@@ -2,5 +2,5 @@ var BCLS_set_date = ( function (window, document) {
   var  bc_copyright = document.getElementById('bc_copyright'),
     c = bc_copyright.textContent,
     y = new Date().getFullYear();
-    bc_copyright.textContent = '&copy;' + y + '&nbsp;' + c;
+    bc_copyright.innerHTML = '&copy;' + y + '&nbsp;' + c;
 })(window, document);
